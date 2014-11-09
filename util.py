135e05fa5b04e1bef56de97ede439cb127e01c28
André Fillipe de Oliveira e Silva <andre@filli.pe>
2014-11-09 14:49:48 -0200

    Update variable levels when indenting and unindenting
    There must be a line selection for this to work.

--- util.py
+++ util.py
@@ -5,6 +5,8 @@ import sublime
 
 VERSION = '0.1.0'
 
+_VAR_LEVEL = re.compile(r'^\s*(\d+).*$')
+
 
 def is_natural_file(view):
     """Is this view editing a Natural file?"""
@@ -24,3 +26,29 @@ def find_text_by_selector(view, selector):
     """Find the text that is "covered" by the given selector."""
     regions = view.find_by_selector(selector)
     return [view.substr(region) for region in regions]
+
+
+def update_var_levels(view, edit, line, amount=+1):
+    """Update the variable levels in the given line. If amount is +1,
+
+    1 #my-var (a10)
+
+    becomes
+
+    2 #my-var (a10)
+
+    Variable levels never go below zero.
+    """
+    text = view.substr(line)
+    match = _VAR_LEVEL.match(text)
+    if not match:
+        return
+    start = match.start(1)
+    end = match.end(1)
+    level_string = match.group(1)
+    new_level = int(level_string, base=10) + amount
+    if new_level < 1:
+        new_level = 1
+    new_level_string = str(new_level)
+    level_region = sublime.Region(line.begin() + start, line.begin() + end)
+    view.replace(edit, level_region, new_level_string)
